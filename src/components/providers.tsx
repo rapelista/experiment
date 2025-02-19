3d242310bf050ae8f05da513769adff5108885ac
Farih Akmal Haqiqi <akmal@gamatecha.com>
2025-02-19 13:10:57 +0700

    Add React Query integration and update ESLint configuration

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,12 +2,23 @@
 
 import { MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { modalsProviderProps, theme } from '~/utils/libs/mantine';
+import { getQueryClient } from '~/utils/libs/query';
 
 export function Providers({ children }: React.PropsWithChildren) {
+  const client = getQueryClient();
+
   return (
     <MantineProvider theme={theme}>
-      <ModalsProvider {...modalsProviderProps}>{children}</ModalsProvider>
+      <ModalsProvider {...modalsProviderProps}>
+        <QueryClientProvider client={client}>
+          {children}
+
+          <ReactQueryDevtools buttonPosition="bottom-left" />
+        </QueryClientProvider>
+      </ModalsProvider>
     </MantineProvider>
   );
 }
